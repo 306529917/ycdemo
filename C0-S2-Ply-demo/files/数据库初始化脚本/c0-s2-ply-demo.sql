720e09e839213f15c48a284303f7f2ec3e2bb7e8
6606 <306529917@qq.com>
2019-11-23 17:21:41 +0800

    1

--- C0-S2-Ply-demo/files/数据库初始化脚本/c0-s2-ply-demo.sql
+++ C0-S2-Ply-demo/files/数据库初始化脚本/c0-s2-ply-demo.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50018
 File Encoding         : 65001
 
-Date: 2019-11-16 15:30:50
+Date: 2019-11-23 17:12:27
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,19 +26,40 @@ CREATE TABLE `books` (
   `pressdate` date default NULL,
   `bookauthor` varchar(40) default NULL,
   `bookcount` int(11) default NULL,
+  `bookimage` varchar(40) default NULL,
   PRIMARY KEY  (`bookid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of books
 -- ----------------------------
-INSERT INTO `books` VALUES ('1', '计算机组成原理', '川大出版社', '2009-09-24', '张永强', '3');
-INSERT INTO `books` VALUES ('2', '数据库', '清华出版社', '1998-10-30', '周杰伦', '2');
-INSERT INTO `books` VALUES ('3', '徐志摩诗集', '永强出版社', '1995-03-04', '徐志摩', '1');
-INSERT INTO `books` VALUES ('4', '高等数学', '晶晶出版社', '2011-07-04', '周传雄', '3');
-INSERT INTO `books` VALUES ('5', '中国历史', '人民出版社', '2016-06-22', '温家宝', '2');
-INSERT INTO `books` VALUES ('6', '盗梦空间', '神话出版社', '2014-08-28', 'mary', '1');
-INSERT INTO `books` VALUES ('7', '西游记', '人民出版社', '2001-12-01', '吴承恩', '17');
-INSERT INTO `books` VALUES ('8', '水浒传', '贵州出版社', '1977-04-04', '施耐庵', '5');
-INSERT INTO `books` VALUES ('9', '红楼梦', '上海出版社', '1966-04-11', '曹雪芹', '8');
-INSERT INTO `books` VALUES ('10', '三国演义', '宇宙无敌出版社', '2001-09-09', '罗贯中', '13');
+INSERT INTO `books` VALUES ('1', '计算机组成原理', '川大出版社', '2009-09-24', '张永强', '3', '../资料/images/计算机组成原理.jpg');
+INSERT INTO `books` VALUES ('2', '数据库', '清华出版社', '1998-10-30', '周杰伦', '2', '../资料/images/数据库.jpg');
+INSERT INTO `books` VALUES ('3', '徐志摩诗集', '永强出版社', '1995-03-04', '徐志摩', '1', '../资料/images/徐志摩诗集.jpg');
+INSERT INTO `books` VALUES ('4', '高等数学', '晶晶出版社', '2011-07-04', '周传雄', '3', '../资料/images/高等数学.jpg');
+INSERT INTO `books` VALUES ('5', '中国历史', '人民出版社', '2016-06-22', '温家宝', '2', '../资料/images/中国历史.jpg');
+INSERT INTO `books` VALUES ('6', '盗梦空间', '神话出版社', '2014-08-28', 'mary', '1', '../资料/images/盗梦空间.jpg');
+INSERT INTO `books` VALUES ('7', '西游记', '人民出版社', '2001-12-01', '吴承恩', '17', '../资料/images/西游记.jpg');
+INSERT INTO `books` VALUES ('8', '水浒传', '贵州出版社', '1977-04-04', '施耐庵', '5', '../资料/images/水浒传.jpg');
+INSERT INTO `books` VALUES ('9', '红楼梦', '上海出版社', '1966-04-11', '曹雪芹', '8', '../资料/images/红楼梦.jpg');
+INSERT INTO `books` VALUES ('10', '三国演义', '宇宙无敌出版社', '2001-09-09', '罗贯中', '13', '../资料/images/三国演义.jpg');
+INSERT INTO `books` VALUES ('14', 'qwe', 'qwe', '2019-11-15', 'qwe', '1', 'upload/08.jpg');
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (
+  `sn` varchar(50) default NULL,
+  `name` varchar(50) default NULL,
+  `age` int(11) default NULL,
+  `grade` int(11) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES ('1', '宋江', '10', '3');
+INSERT INTO `student` VALUES ('2', '林冲', '28', '3');
+INSERT INTO `student` VALUES ('3', '鲁达', '26', '3');
+INSERT INTO `student` VALUES ('4', '花荣', '22', '3');
