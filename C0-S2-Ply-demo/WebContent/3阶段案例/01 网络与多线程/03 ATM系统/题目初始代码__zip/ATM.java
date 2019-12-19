a3e4d4702b98e6a22cf5b8e22c2ffb907bece7a7
2505 <306529917@qq.com>
2019-12-19 16:56:41 +0800

    1

---
+++ C0-S2-Ply-demo/WebContent/3阶段案例/01 网络与多线程/03 ATM系统/题目初始代码__zip/ATM.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * 银行柜员机类
+ *
+ */
+public class ATM {
+
+	private Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		new ATM().start();
+	}
+
+	public void start() {
+		int op = 0;
+		boolean ok = false;
+		while (ok == false) {
+			System.out.println("******************************");
+			System.out.println("*           我的银行                 *");
+			System.out.println("******************************");
+			System.out.println("*           1、登录                   *");
+			System.out.println("*           2、开户                   *");
+			System.out.println("******************************");
+			System.out.print("请输入你要办理的业务编号：");
+			op = sc.nextInt();
+			switch (op) {
+			case 1:
+				if ((ok = login()) == false) {
+					System.out.println("密码错误！");
+				}
+				break;
+			case 2:
+				register();
+				break;
+			}
+		}
+
+		while (true) {
+			System.out.println("\n");
+			System.out.println("******************************");
+			System.out.println("*           我的银行                 *");
+			System.out.println("******************************");
+			System.out.println("*         1、查看余额                *");
+			System.out.println("*         2、存       款                *");
+			System.out.println("*         3、取       款                *");
+			System.out.println("*         4、转       账                *");
+			System.out.println("*         5、查询明细                *");
+			System.out.println("*         0、退       出                *");
+			System.out.println("******************************");
+			System.out.print("请输入你要办理的业务编号：");
+			op = sc.nextInt();
+			switch (op) {
+			case 0:
+				System.exit(0);
+			case 1:
+				deposit();
+				break;
+			case 2:
+				withdraw();
+				break;
+			case 3:
+				transfer();
+				break;
+			default:
+				System.out.println("输入错误，请求重新输入！");
+			}
+		}
+
+	}
+
+	private void register() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void transfer() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void deposit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void withdraw() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private boolean login() {
+		return true
+				;
+	}
+
+}
---
+++ C0-S2-Ply-demo/WebContent/3阶段案例/01 网络与多线程/03 ATM系统/题目初始代码__zip/Bank.java
@@ -0,0 +1,11 @@
+
+/**
+ * 银行类
+ */
+public class Bank {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
