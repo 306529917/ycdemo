0d32cd7033c03253dbc0d0a16787ed7d56940dd0
liaoyan <liaoyan@chinasofti.com>
2020-01-09 22:47:51 +0800

    1

---
+++ C0-S2-Ply-demo/WebContent/3阶段案例/07 SpringCloud/02 服务熔断与降级/02 参考代码/C0-S3-Ply-cloud-index/src/main/java/com/yc/C0S3Plycloudindex/C0S3PlyCloudIndexApplication.java
@@ -0,0 +1,27 @@
+package com.yc.C0S3Plycloudindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+//step 2
+@EnableEurekaClient
+public class C0S3PlyCloudIndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C0S3PlyCloudIndexApplication.class, args);
+	}
+	
+	// step 1
+	@Bean
+	// step 2
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
