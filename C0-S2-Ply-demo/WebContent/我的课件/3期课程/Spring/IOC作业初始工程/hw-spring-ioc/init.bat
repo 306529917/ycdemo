3d238b0c216944612ace520629477f0d7e843dcf
liaoyan <liaoyan@chinasofti.com>
2020-02-20 01:14:21 +0800

    1

--- C0-S2-Ply-demo/WebContent/我的课件/3期课程/Spring/IOC作业初始工程/hw-spring-ioc/init.bat
+++
@@ -1,2 +0,0 @@
-del /q src\main\java\com\yc\spring\*.*
-del /q src\main\resources\*.*
--- C0-S2-Ply-demo/WebContent/我的课件/3期课程/Spring/IOC作业初始工程/hw-spring-ioc/src/main/java/com/yc/spring/bbs/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.yc.spring.bbs.dao;
-
-import com.yc.spring.bbs.bean.User;
-
-public interface  UserDao extends BaseDao<User> {
-
-}
