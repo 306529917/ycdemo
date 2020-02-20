0dc6509e0035d407aaaf5446b57a1ebfdefa1a62
6606 <306529917@qq.com>
2020-02-20 08:28:16 +0800

    1

--- LWJ/src/Classical.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Classical {
-	private static Scanner s;
-	public static void main(String[] args) {
-		s=new Scanner(System.in);
-		int x=s.nextInt();
-	}
-}
