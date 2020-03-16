b8cad31c857c99292f41273691c7b885da13495a
6606 <306529917@qq.com>
2020-03-16 09:36:13 +0800

    1

--- C0-S2-Ply-demo/WebContent/_项目/C0-S3-Ply-game/src/main/java/com/yc/game/wuzi/Main.java
+++ C0-S2-Ply-demo/WebContent/_项目/C0-S3-Ply-game/src/main/java/com/yc/game/wuzi/Main.java
@@ -6,16 +6,25 @@ import com.yc.game.wuzi.core.WuziGame;
 import com.yc.game.wuzi.core.WuziGameImpl;
 import com.yc.game.wuzi.swing.MainWin;
 
+/**
+ * 五子棋主程序
+ * @author 廖彦
+ */
+
 public class Main {
-	/**
-	 * Launch the application.
-	 * 这是自动生成的代码, 用于启动窗口
-	 */
 	public static void main(String[] args) {
+		// 传入游戏实现类对象
 		start(new WuziGameImpl());
 	}
 	
+	/**
+	 * 开始游戏
+	 * @param game
+	 */
 	public static void start(WuziGame game) {
+		/**
+		 * 这是自动生成的代码, 用于启动窗口
+		 */
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
--- C0-S2-Ply-demo/WebContent/_项目/C0-S3-Ply-game/src/main/java/com/yc/game/wuzi/swing/MyLabel.java
+++ C0-S2-Ply-demo/WebContent/_项目/C0-S3-Ply-game/src/main/java/com/yc/game/wuzi/swing/MyLabel.java
@@ -3,25 +3,33 @@ package com.yc.game.wuzi.swing;
 import javax.swing.Icon;
 import javax.swing.JLabel;
 
+/**
+ * 自定义 Label 类，继承于JLabel， 扩展了棋盘坐标属性<br>
+ * 用于确定当前 Label 与棋盘上过的哪个坐标点是相关联的
+ * @author 廖彦
+ *
+ */
 public class MyLabel extends JLabel{
 
 	private static final long serialVersionUID = 1L;
 	
+	// 棋盘上对应的坐标值
 	private int x, y;
 
 	public int getBoardX() {
 		return x;
 	}
 
-	public void setXY(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-
 	public int getBoardY() {
 		return y;
 	}
 
+	/**
+	 * 构建对象时,传入坐标值 x,y
+	 * @param image
+	 * @param x
+	 * @param y
+	 */
 	public MyLabel(Icon image, int x, int y) {
 		super(image);
 		this.x = x;
