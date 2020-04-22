188d113c0e18cefcea016e0028ff18090d900bdb
6606 <306529917@qq.com>
2020-04-22 17:19:51 +0800

    1

--- C0-S2-Ply-demo/src/com/yc/jee/图书管理/servlet/TestServlet.java
+++ C0-S2-Ply-demo/src/com/yc/jee/图书管理/servlet/TestServlet.java
@@ -3,6 +3,7 @@ package com.yc.jee.图书管理.servlet;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,7 +28,10 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.addCookie(new Cookie("mmm","nnnn"));
+		request.getRequestDispatcher("/_项目/JS/a.html")
+		.forward(request, response);
 	}
 
 	/**
