ee2591e36a8aad1d14c78b11619e280088e3b0a4
6606 <306529917@qq.com>
2020-05-26 13:26:46 +0800

    1

--- C0-S2-Ply-demo/WebContent/_js/pub1.js
+++ C0-S2-Ply-demo/WebContent/_js/pub1.js
@@ -13,75 +13,30 @@ Vue.component('ipt',{
 	template : `<span class='releaseMe qspan'><input :answer='a' :regex='r' style="text-align:center" :size='w&&w.indexOf("px")>-1?false:w' :style='{width:w&&w.indexOf("px")>-1?w:false}'><span>`
 });
 
-var rdoNameIndex = 0;
-Vue.component('cbx',{
-	data : function(){
-		return {
-			n : "cor-" + ++rdoNameIndex,
-			newa : "",
-		};
-	},
-	// v=>value, a=>answer, s=>新行前导, o=>不打乱顺序
-	props : ["v","a","s","o"],
-	methods : {
-		rand(v,a,o){
-			// o有值则不打乱
-			if(o!=undefined) {
-				this.newa = a;
-				return v;
-			}
-			/** 打乱顺序 */
-			let answers = [];
-			let astring = Array.isArray(a) ? a.join(",") : a;
-			// 保存正确答案
-			v.forEach((o,i)=>{
-				if(astring.indexOf(i)>-1){
-					answers.push(o);
-				}
-			});
-			// 打乱数组
-			v.sort(function(){
-				let n = Math.random();
-				return  n > 0.5 ? 1 : -1; 
-			});
-			astring = "";
-			// 重新构建答案字符串
-			v.forEach((o,i)=>{
-				let pos = answers.indexOf(o);
-				if(pos > -1){
-					astring += i;
-				}
-			});
-			this.newa = astring;
-			return v;
-		}
-	},
-	template : `<span class='releaseMe qspan' v-if="rand(v,a,o)"><slot></slot>`
-		+`<span class='releaseMe' v-for='(r,i) in v'>`
-		+`<input :type='a.length==1?"radio":"checkbox"' :id='n+"-"+i' :name='n' :value='r' :answer='newa.indexOf(i)>-1 ? r : false'>`
-		+`<label :for='n+"-"+i' style='padding-right:22px' class="removeMe">{{r}}</label>`
-		+`<br v-if='s!=undefined && i<v.length-1'>{{s}}`
-		+`</span></span>`
-});
-
 function toArray(obj,spacer){
 	if(obj){
 		if(Array.isArray(obj)){
 			return obj;
 		} else {
 			obj += "";
-			spacer = spacer ? spacer : "[;,，；]+";
-			return obj.split(new RegExp(spacer,"g"));
+			spacer = spacer ? spacer : "[;,，；、]+";
+			let ret = obj.split(new RegExp(spacer,"g"));
+			ret.forEach((r,i)=>{
+				ret[i] = r.replace(/(^\s+|\s+$)/g, "");
+			});
+			return ret;
 		}
 	} else {
 		return [];
 	}
 }
 
+var isTeach = location.hash == "#teach";
+var	sn = "ABCDEFGHIJK";
 var cNameIndex=0;
 chtml = `<span class='releaseMe' v-for='(r,i) in values'>{{s}}`
-	+`<input :type='type' :id='name+"-"+i' :name='name' :value='r.value' :answer='r.isTrue?r.value:false'>`
-	+`<label :for='name+"-"+i' style='padding-right:22px' class="removeMe">{{r.value}}</label>`
+	+`<input v-if="!isTeach" :type='type' :id='name+"-"+i' :name='name' :value='r.value' :answer='r.isTrue?r.value:false'>`
+	+`<label :for='name+"-"+i' style='padding-right:22px' class="removeMe">{{values.length>2?sn[i]+". ":""}}{{r.value}}</label>`
 	+`<br v-if='s!=undefined && i<v.length-1'>`
 	+`</span></td></tr>`;
 Vue.component('c',{
@@ -90,86 +45,70 @@ Vue.component('c',{
 			name : "c-" + ++cNameIndex,
 			type : "radio",
 			values : [],
+			isJuage : false,
+			slotText : null
 		};
 	},
 	// v=>value, a=>answer 从1开始计数, s=>答案换行新行前导, spacer=>答案分隔符
-	props : ["v","a","s","spacer"],
+	props : ["v","a","s","w","spacer","noteach"],
 	methods : {
-		init(v,a,s,spacer){
+		init(v,a,s,w,spacer,noteach){
+			if(noteach != undefined){
+				return false;
+			}
+			// 获取slot的值, 去掉首位空格
+			let st = this.$slots.default[0].text;
+			st = st.replace(/(^\s+|\s+$)/g,"");
+			this.slotText = st;
+			// 默认正确答案是第一个选项
 			a = a || "1";
-			// 不提供, 则为判断题
-			if( ! v ){
-				this.values[0] = {value:"是"};
-				this.values[1] = {value:"否"};
-				let tIndex = ( a && a.indexOf("2")>-1 ) ? 1 : 0;
-				this.values[tIndex].isTrue = true;
+			// v不提供, 则为判断题
+			if( ! v){
+				this.isJuage = true;
+				// 教学模式不生成控件
+				if(!isTeach){
+					this.values[0] = {value:"是"};
+					this.values[1] = {value:"否"};
+					let tIndex = ( a && a.indexOf("2")>-1 ) ? 1 : 0;
+					this.values[tIndex].isTrue = true;
+				}
 			} else {
 				let tCount = 0;
-				v = toArray(v);
+				v = toArray(v,spacer);
 				let values = [];
 				v.forEach((val,i)=>{
 					if(val){
-						val = val.replace(/(^\s+|\s+$)/g,"");
 						let vobj = {};
 						vobj.value = val;
 						vobj.isTrue = a.indexOf(i+1) > -1;
 						tCount += vobj.isTrue ? 1 : 0;
-						values.push(vobj);
+						// 教学模式不添加错误答案
+						if(vobj.isTrue || ! isTeach){
+							values.push(vobj);
+						}
 					}
 				});
 				this.type = tCount == 1 ? "radio" : "checkbox";
-				values.sort((a,b)=>{
-					return Math.random() > 0.5 ? 1 : -1; 
-				});
+				// 教学模式不生成控件, 不排序
+				if(!isTeach){
+					values.sort((a,b)=>{
+						return Math.random() > 0.5 ? 1 : -1; 
+					});
+				}
 				this.values = values;
 			}
 			return true;
 		}
 	},
-	template : `<table class='releaseMe qspan' v-if="init(v,a,s,spacer)">`
-		+`<tr v-if="values.length<3"><td><slot></slot></td>`
+	template : `<table class='releaseMe qspan' v-if="init(v,a,s,w,spacer,noteach)" :style="{width:w?w:'100%'}">`
+		+`<tr v-if="isJuage"><td>{{slotText}}</td>`
 		+`<td style="width:150px;text-align:center">`
 		+ chtml
-		+`<tr v-if="values.length>=3"><td><slot></slot></td></tr>`
-		+`<tr v-if="values.length>=3"><td>`
+		+`<tr v-if="!isJuage"><td>{{slotText}}</td></tr>`
+		+`<tr v-if="!isJuage"><td>`
 		+ chtml
 		+`</table>`
 })
-
-Vue.component('judge',{
-	data : function(){
-		return {
-			n : "jdg-" + ++rdoNameIndex,
-			neww : "400px"
-		};
-	},
-	methods : {
-		set(w){
-			if(w == undefined){
-				this.neww = false;
-			} else {
-				w = w.replace(/\s/g, "");
-				if(w>""){
-					if (w.indexOf("px")>0){
-						this.neww = w;
-					} else {
-						this.neww = w + "px";
-					}
-				}
-			}
-			return true;
-		}
-	},
-	props : ["t","f","w"], // w 不写=>无, 写无值=>400
-	template : `<span class='releaseMe qspan' v-if="set(w)">`
-			+`<span class='releaseMe' :style='{display:w==undefined?false:"inline-block",width:neww}'>`
-			+`<slot>{{t||f}}</slot></span>`
-			+`<input type='radio' :id='n+"-0"' :name='n' value='是' :answer='t!=undefined || t==undefined && f==undefined ?"是":false'>`
-			+`<label :for='n+"-0"' style='padding-right:22px' class="removeMe">是</label>`
-			+`<input type='radio' :id='n+"-1"' :name='n' value='否' :answer='f!=undefined?"否":false'>`
-			+`<label :for='n+"-1"' style='padding-right:22px' class="removeMe">否</label>`
-			+`</span>`
-});
 
 Vue.component('prompt',{
 	template : `
@@ -192,17 +131,17 @@ Vue.component('prompt',{
 });
 
 Vue.component('pic',{
-	props : [ "s", "w"],
-	template : `<div style="
-				clear:both;
+	props : [ "s", "w", "prefix"],
+	template : `<div><div v-for="p in toArray(s)" v-if="p" style="
+				float:left;
 				display:inline-block;
 				text-align:center;
 				margin:10px;
 				border-radius: 5px;
 				border:1px solid #888"
 				:style="{width:w?w.indexOf('px')>0?w:(w+'px'):'150px'}">`
-		+`<a :href="s" class='MagicZoom'>`
-		+`<img :src="s" style="background-color:#fff">`
+		+`<a :href="(prefix?prefix:'_images/') + p" class='MagicZoom'>`
+		+`<img :src="(prefix?prefix:'_images/') + p" style="background-color:#fff">`
 		+`</a>`
-		+`<br><slot></slot></div>`
+		+`<br>{{p.replace(/(\\d+-)*(.+)+?\\.\\w+/,"$2")}}</div></div>`
 });
