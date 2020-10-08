d5de42f9f2985784e78a8399e59cfb115186b9e4
6606 <306529917@qq.com>
2020-10-08 09:00:19 +0800

    1

---
+++ C0-S2-Ply-demo/WebContent/WEB案例模板/网站原型/超高无损音乐/相关资料/bean/SqAlbum.java
@@ -0,0 +1,43 @@
+package com.yc.demo.example.sq.bean;
+
+public class SqAlbum {
+    private Integer id;
+
+    private Integer singerId;
+
+    private Integer songId;
+
+    private String url;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSingerId() {
+        return singerId;
+    }
+
+    public void setSingerId(Integer singerId) {
+        this.singerId = singerId;
+    }
+
+    public Integer getSongId() {
+        return songId;
+    }
+
+    public void setSongId(Integer songId) {
+        this.songId = songId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+}
